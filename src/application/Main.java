cdebcaf203a6244fac4c4a94cf74dc9272d5b13c
minkyu.kim2 <minkyu.kim2@IC-CS233-6.uc.ucalgary.ca>
2022-07-19 16:59:38 -0600

    Added my branch label.

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label("David's label"));
+			root.getChildren().add(new Label("Minkyu's branch"));
 			Scene scene = new Scene(root,400,400);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
